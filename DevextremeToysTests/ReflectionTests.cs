02f7288bb5b162b7f10b017cb5160e5df41fe459
AndreaPic <andrea.piccioni@libero.it>
2022-05-26 22:01:01 +0200

    added more reflection

--- DevextremeToysTests/ReflectionTests.cs
+++ DevextremeToysTests/ReflectionTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +11,12 @@ namespace DevExtremeToysTests
 {
     public class ReflectionTests
     {
+        [Serializable]
         public class ReflectionTestClass
         {
             public string Name { get; set; }
             public string Description { get; set; }
+            [Range(0,int.MaxValue)]
             public int Id { get; set; }
         }
 
@@ -33,8 +36,25 @@ namespace DevExtremeToysTests
             Assert.Equal(myClass.GetPropertyValue(nameof(ReflectionTestClass.Name)) , "My Name");
             Assert.Equal(myClass.GetPropertyValue(nameof(ReflectionTestClass.Description)), "My Description");
             Assert.Equal(myClass.GetPropertyValue(nameof(ReflectionTestClass.Id)), 10);
-
-
         }
+
+        [Fact]
+        public void AttributesTest()
+        {
+            var classAttrs = typeof(ReflectionTestClass).GetAttributes();
+            Assert.NotNull(classAttrs);
+            Assert.True(classAttrs.Count() > 0);
+
+            var attr = typeof(ReflectionTestClass).GetAttribute<SerializableAttribute>();
+            Assert.NotNull(attr);
+
+            var pAttr = typeof(ReflectionTestClass).GetPropertyAttribute<RangeAttribute>(nameof(ReflectionTestClass.Id));
+            Assert.NotNull(pAttr);
+
+            var pNames = typeof(ReflectionTestClass).GetPropertyNames();
+            Assert.NotNull(pNames);
+            Assert.True(pNames.Count() > 0);
+        }
+
     }
 }
